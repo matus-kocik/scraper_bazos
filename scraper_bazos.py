4f0b27712c088c0d0635939c746b23f54a4541ce
Matúš Kočik <matuskocik@gmail.com>
2023-07-30 11:46:04 +0200

    add headers

--- scraper_bazos.py
+++ scraper_bazos.py
@@ -1,3 +1,5 @@
 import requests
 
 URL = 'https://reality.bazos.sk/inzerat/153724690/2-izbovy-svetly-byt-v-tichej-lokalite-ul-karpatska-sekcov.php'
+
+headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
