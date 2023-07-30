a8041dcd45623217c551a176630ba00e08e728da
Matúš Kočik <matuskocik@gmail.com>
2023-07-30 13:58:57 +0200

    change variable headers, from headers to my_headers, better for me, get() attribute headers / variable my_headers

--- scraper_bazos.py
+++ scraper_bazos.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 
 URL = 'https://reality.bazos.sk/inzerat/153724690/2-izbovy-svetly-byt-v-tichej-lokalite-ul-karpatska-sekcov.php'
 
-headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
+my_headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
 
-page = requests.get(URL, headers=headers)
+page = requests.get(URL, headers=my_headers)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 
