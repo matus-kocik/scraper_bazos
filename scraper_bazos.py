e7b68b6ad84334937c4e2142de0fb4515ed9a0af
Matúš Kočik <matuskocik@gmail.com>
2023-07-30 17:31:34 +0200

    add function def check_price()...

--- scraper_bazos.py
+++ scraper_bazos.py
@@ -6,30 +6,31 @@ URL = 'https://reality.bazos.sk/inzerat/153724690/2-izbovy-svetly-byt-v-tichej-l
 
 my_headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
 
-page = requests.get(URL, headers=my_headers)
+def check_price():
+    page = requests.get(URL, headers=my_headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-# print(soup.prettify())
+    # print(soup.prettify())
 
-title = soup.find(class_="nadpisdetail").get_text()
-# price = soup.find(id="" or class_="").get_text()
-# converted_price = float(price[0:5]) - not realy for me
-# print(converted_price)
+    title = soup.find(class_="nadpisdetail").get_text()
+    # price = soup.find(id="" or class_="").get_text()
+    # converted_price = float(price[0:5]) - not realy for me
+    # print(converted_price)
 
-print(title.strip())
+    print(title.strip())
 
-price_string = soup.find(string="Cena:")
+    price_string = soup.find(string="Cena:")
 
-if price_string:
-    price_element = price_string.find_next('b')
-    price = price_element.get_text().strip()
-else:
-    print("Price not found on the website.")
+    if price_string:
+        price_element = price_string.find_next('b')
+        price = price_element.get_text().strip()
+    else:
+        print("Price not found on the website.")
 
-converted_price = float(price.replace("€", "").replace(" ", "").replace(",", "."))
+    converted_price = float(price.replace("€", "").replace(" ", "").replace(",", "."))
 
-if (converted_price > 99991):
-    send_mail()
-else:
-    print("Price is not change!")
+    if (converted_price > 99991):
+        send_mail()
+    else:
+        print("Price is not change!")
